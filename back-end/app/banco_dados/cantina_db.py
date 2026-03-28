dabd31542ed303d6211302fce28958023dc2a2cb
Herbert <herbertpierre779@gmail.com>
2026-03-27 21:09:26 -0300

    Sem foto e outras oicsas

--- back-end/app/banco_dados/cantina_db.py
+++ back-end/app/banco_dados/cantina_db.py
@@ -1,26 +1,43 @@
 import sqlite3
 
-conn = sqlite3.connect(r'C:\Users\fa285\Downloads\cantina\back-end\app\banco_dados\cantina.db')
+conn = sqlite3.connect(r"C:\Users\fa285\Downloads\cantina\back-end\data\cantina.db")
 
 cursor = conn.cursor()
 
 print("Conectado ao SQLite ")
 
-
-
-cursor.execute("""CREATE TABLE IF NOT EXISTS alunos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    cpf INTEGER UNIQUE NOT NULL,
-    turma VARCHAR(50),
-    sala VARCHAR(10)
+cursor.execute("""CREATE TABLE IF NOT EXISTS salas(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               name VARCHAR(10) NOT NULL,
+               school_year VARCHAR(10) NOT NULL,
+               created_at VARCHAR(27),
+               updated_at VARCHAR(27),
 )""")
 
+
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS alunos(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               cpf VARVHAR(11) UNIQUE,
+               nome TEXT,
+               class_id VARCHAR(32),
+               photo_pat VARCHAR(100),
+               created_at VARCHAR(27),
+               updated_at VARCHAR(27),
+)""")
+               
+cursor.execute("""CREATE TABLE IF NOT EXISTS usuarios(
+               id INTERGER PRIMARY KEY AUTOINCREMENT,
+               username
+               
+
+ )""")
+
 cursor.execute(
-    "INSERT INTO usuarios (nome, cpf, turma, sala) VALUES (?,?,?,? )",
-    #pierre acrecenta as variaveis aqui para adicionar um aluno
-    (nome, cpf, turma, sala)
-)
+     "INSERT INTO usuarios (nome, cpf, turma, sala) VALUES (?,?,?,? )",
+     #pierre acrecenta as variaveis aqui para adicionar um aluno
+     (nome, cpf, turma, sala)
+ )
 
 
 
